55fe3d54ed557a01f696f8987d8f0b60b5f150d1
vivizco <dawidmg@gmail.com>
2018-07-30 14:17:11 +0200

    Test2

--- Szkolenie/Controllers/HomeController.cs
+++ Szkolenie/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Szkolenie.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page 2.";
 
             return View();
         }
