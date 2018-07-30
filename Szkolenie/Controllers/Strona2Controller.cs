b0327530e6ad10a4da4b0113f169aed64e0d1b9c
Krzysiek <Krzysiek@BIURO>
2018-07-30 14:53:23 +0200

    cos

---
+++ Szkolenie/Controllers/Strona2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Szkolenie.Controllers
+{
+    public class Strona2Controller : Controller
+    {
+        // GET: Strona2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
